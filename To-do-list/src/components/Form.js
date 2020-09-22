b52359ff4adf414dc54f310cba8306e9d21db09b
Ali Adrian <alinasrat95@gmail.com>
2020-09-23 00:50:41 +0200

    to-do-list-project

---
+++ To-do-list/src/components/Form.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function Form({setInputText, inputText, todos, setTodos, setStatus}) {
+    // Here I can write JavaScript code and functions
+    const inputTextHandler = (e) => {
+        setInputText(e.target.value);
+    };
+    
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+
+        if(inputText === "") {
+            return;
+        } else {
+
+            setTodos([
+                ...todos, 
+                { text: inputText, completed: false, id: Math.random() * 1000 }
+            ]);
+        }
+        setInputText("");
+    }
+
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    }
+    return (
+            <form>
+            <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    )
+}
